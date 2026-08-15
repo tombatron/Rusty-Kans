39c11fead003f1321bcafe0bb07e0e98b2365b16
Tom Hanks <tommy.hanks@gmail.com>
2026-08-15 11:15:18 -0400

    Added a bit more test coverage.

--- src/handlers/utility.rs
+++ src/handlers/utility.rs
@@ -11,3 +11,15 @@ pub fn get_router_configuration() -> Router<ApplicationState> {
 pub async fn get_health() -> Json<Value> {
     Json(json!({"status": "ok"}))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::handlers::utility::get_health;
+
+    #[tokio::test]
+    async fn get_health_returns_health_response() {
+        let response = get_health().await.0;
+
+        assert_eq!("ok", response["status"]);
+    }
+}
--- src/handlers/web.rs
+++ src/handlers/web.rs
@@ -7,7 +7,7 @@ use crate::models::Board;
 use crate::state::ApplicationState;
 use askama::Template;
 use axum::extract::State;
-use axum::response::{Html, IntoResponse};
+use axum::response::Html;
 use axum::routing::get;
 use axum::Router;
 use crate::data;
@@ -27,10 +27,31 @@ struct LandingTemplate {
     boards: Vec<Board>
 }
 
-async fn get_landing(State(state): State<ApplicationState>) -> Result<impl IntoResponse, KanbanError> {
+async fn get_landing(State(state): State<ApplicationState>) -> Result<Html<String>, KanbanError> {
     let boards = data::get_all_boards(&state.db).await?;
 
     let template = LandingTemplate { boards };
     
     Ok(Html(template.render()?))
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::extract::State;
+    use sqlx::SqlitePool;
+    use crate::handlers::tests::get_fake_application_state;
+    use crate::handlers::web::get_landing;
+
+    #[sqlx::test(fixtures(path="../fixtures", scripts("boards")))]
+    async fn get_landing_returns_all_boards(db: SqlitePool) -> sqlx::Result<()> {
+        let state = State(get_fake_application_state(db));
+
+        let response = get_landing(state).await.unwrap().0;
+
+        assert!(response.contains("board-1"));
+        assert!(response.contains("board-2"));
+        assert!(response.contains("board-3"));
+
+        Ok(())
+    }
 }
